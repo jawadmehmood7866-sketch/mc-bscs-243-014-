d794cecbac17e2754d430e893fed3e5262019706
jawadmehmood7866-sketch <jawadmehmood7866@gmail.com>
2025-11-02 18:19:54 +0500

    Add files via upload

---
+++ markdown.py
@@ -0,0 +1,42 @@
+print("Hello World")
+
+print(3)
+
+print(3,2,1)
+
+print(3+2+1)
+
+print("name ",3+3)
+
+
+A = 22
+B = 33
+C = A + B
+print(C)
+
+age = 22
+name = "jawad"
+print(name,":",age)
+print(age)
+
+A,B,C = "Orange","Banana","Apple"
+print(A,B,C)
+
+Fruit = ["Orange","Banana","Apple"]
+print(Fruit)
+print(type(Fruit))
+
+X=range(10)
+print(X)
+print(type(X))
+
+Student={
+    "Name":"jawad mehmood",
+    "Reg#":"MC-BSCS-243-014",
+    "CGPA":"3.75",
+    "Email":"jawad@example.com",
+    "Phone":"123-456-7890",
+    "Address":"123 Main St, Multan, Pakistan",
+}
+print(Student)
+
